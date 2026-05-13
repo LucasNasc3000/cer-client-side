1bb71bcc45c7b16ef04efedb487245af9696ba8f
lucasNasc <lucasfortunato328@gmail.com>
2026-05-13 18:04:53 -0300

    adicionando elementos e salvamento local de dados dos ingredientes

--- src/components/ModalRecipes/addRecipe.js
+++ src/components/ModalRecipes/addRecipe.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-case-declarations */
 /* eslint-disable jsx-a11y/control-has-associated-label */
+import Decimal from "decimal.js";
 import { get } from "lodash";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
@@ -9,8 +11,10 @@ export function ModalRecipeChildren() {
   const [inputSearchValue, setInputSearchValue] = useState("");
   // eslint-disable-next-line no-unused-vars
   const [quantity, setQuantity] = useState("");
+  const [unitOrWeight, setUnitOrWeight] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const recipeItems = [];
+  const recipeItemsToShow = [];
 
   // Exibindo as receitas conforme for salvando e botão de cancelar para limpar os campos (pesquisa e quantidade)
   useEffect(() => {
@@ -50,9 +54,52 @@ export function ModalRecipeChildren() {
     SearchSupply();
   }, [inputSearchValue]);
 
+  // Converter unidades inteiras (segundo funcionários) para gramas (backend aceita)
+
+  const ClerDirectExecution = () => {
+    setQuantity("");
+    setInputSearchValue("");
+    setSearchResults([]);
+  };
+
   const AddToRecipe = (e, supply) => {
+    let formattedQuantity = "";
+
+    // eslint-disable-next-line default-case
+    switch (unitOrWeight) {
+      case !unitOrWeight:
+        toast.error("Tipo de quantidade não selecionada");
+        return;
+
+      case "unit":
+        const unities = new Decimal(quantity);
+        const toGrams = unities.mul(supply.weightPerUnit).toString();
+        formattedQuantity = toGrams;
+        break;
+
+      case "g" || "ml":
+        formattedQuantity = quantity;
+        break;
+
+      case "kg" || "L":
+        const currentFormat = new Decimal(quantity);
+        const toGramUnit = currentFormat.mul(1000);
+        formattedQuantity = toGramUnit;
+    }
+
     e.preventDefault();
-    recipeItems.push(supply);
+    recipeItems.push({
+      supplyId: supply.id,
+      quantity: formattedQuantity,
+    });
+
+    recipeItemsToShow.push({
+      supplyId: supply.id,
+      name: supply.name,
+      quantity: formattedQuantity,
+    });
+
+    ClerDirectExecution();
   };
 
   return (
@@ -74,7 +121,38 @@ export function ModalRecipeChildren() {
         </ul>
       )}
 
+      <div className="filter-space">
+        <p className="filter-select-label">Selecionar medida:</p>
+        <select
+          name="search-options"
+          className="options"
+          id="filter-select"
+          onChange={(e) => setUnitOrWeight(e.target.value)}
+        >
+          <option value="">Selecione</option>
+          <option value="unit">unidade</option>
+          <option value="g">g</option>
+          <option value="ml">ml</option>
+          <option value="kg">kg</option>
+          <option value="L">L</option>
+        </select>
+      </div>
+
+      {recipeItemsToShow.length > 0 &&
+        recipeItemsToShow.map((item) => (
+          <div className="supply-list">
+            <div className="label">insumo:</div>
+            <div className="name">{item.name}</div>
+            <div className="label">quantidade</div>
+            <div className="quantity">
+              {item.quantity + unitOrWeight !== "unit" && unitOrWeight}
+            </div>
+          </div>
+        ))}
+
       <input type="text" onChange={(e) => setQuantity(e.target.value)} />
+      <button type="button">Adicionar</button>
+      <button type="button">Cancelar</button>
       <button type="button">Salvar</button>
     </ModalRecipeContainer>
   );
