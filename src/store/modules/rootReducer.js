380ac7588a0599c92fe6e8709c282a56f72aa9df
lucasNasc <lucasfortunato328@gmail.com>
2026-06-11 18:08:41 -0300

    adicionando reducer de editar status de venda ao rootReducer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 
 import auth from "./auth/reducer";
 import dataTransfer from "./dataTransfer/reducer";
+import editSalesStatus from "./editSalesStatus/reducer";
 import editUnitiesData from "./editUnitiesData/reducer";
 import recipeData from "./recipeData/reducer";
 import saleItems from "./saleItems/reducer";
@@ -12,6 +13,7 @@ export default combineReducers({
   recipeData,
   editUnitiesData,
   saleItems,
+  editSalesStatus,
 });
 
 // Todos os reducers da aplicação serão importados e combinados aqui
